7ff8e33188eb4b7c78ceb026121956ef6297bdf7
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2022-04-21 20:42:34 -0500

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,6 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KQ8gpQaWY0VTnKTHqHzNfMPV30Qz-IaKbJaLxBKLjPQ/edit#gid=957210929';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBOhOxQmDduXiOjls9aOZ4VmNM4Sx2K-Vw';
+
---
+++ sketchfab.js
@@ -0,0 +1,8 @@
+var sketchfab = {
+"type": "FeatureCollection",
+"name": "sketchfab",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "La Bomba", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -99.195205, 19.113425] } },
+]
+};
